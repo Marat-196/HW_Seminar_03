a93eb85c764af7f8fa7589265bb92e28cb378be9
Marat <zhumataev.marat@bk.ru>
2023-09-25 13:24:01 +0500

    add Task_01

---
+++ Task_01/Program.cs
@@ -0,0 +1,22 @@
+﻿bool IsPalindrome(int number)
+{
+    if (number < 10000 || number > 99999)
+    {
+        Console.WriteLine("Число не пятизначное");
+        return false;
+        
+    }
+    else
+    {
+        int numFirstEnd = number % 10;
+        int numFirstStart = number / 10000;
+        int numSecondEnd = number % 100 / 10;
+        int numSecondStart = number / 1000 % 10;
+        if (numFirstEnd == numFirstStart && numSecondEnd == numSecondStart) return true;
+        else return false;
+    }
+}
+
+// Console.WriteLine("Введите число: ");
+// int val = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(IsPalindrome(val));
